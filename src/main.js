bef8b7d6ca0616f133b3886a6d91bcd5ecbe1352
drafff <i0.0ia@qq.com>
2022-09-19 21:23:43 +0800

    change

--- src/main.js
+++ src/main.js
@@ -1,24 +1,38 @@
 
 import * as THREE from 'three';
-import { TeapotGeometry } from 'three/examples/jsm/geometries/TeapotGeometry';
-import { RoundedBoxGeometry } from 'three/examples/jsm/geometries/RoundedBoxGeometry';
 import SceneInit from './lib/SceneInit';
+import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
+import diff from '/src/textures/painted_concrete_diff_1k.jpg'
+import disp from '/src/textures/painted_concrete_disp_1k.png'
+import nor from '/src/textures/painted_concrete_nor_gl_1k.jpg'
+import rough from '/src/textures/painted_concrete_rough_1k.jpg'
+
+
 const test = new SceneInit('myThreeJsCanvas');
 test.initialize();
 test.animate();
-const roundedBoxGeometry = new RoundedBoxGeometry(1, 1, 1, 4, 0.1);
-const roundedBoxMaterial = new THREE.MeshNormalMaterial({
-  wireframe: true,
-});
-const roundedBoxMesh = new THREE.Mesh(
-  roundedBoxGeometry,
-  roundedBoxMaterial
-);
-roundedBoxMesh.position.x = -1;
-test.scene.add(roundedBoxMesh);
-
-const teapotGeometry = new TeapotGeometry(0.5, 8);
-const teapotMaterial = new THREE.MeshNormalMaterial({ wireframe: true });
-const teapotMesh = new THREE.Mesh(teapotGeometry, teapotMaterial);
-teapotMesh.position.x = 1;
-test.scene.add(teapotMesh);
+const loader = new GLTFLoader()
+const textureLoader = new THREE.TextureLoader()
+const diffTexture = textureLoader.load(diff)
+const dispTexture = textureLoader.load(disp)
+const norTexture = textureLoader.load(nor)
+const roughTexture = textureLoader.load(rough)
+const material = new THREE.MeshBasicMaterial({
+    side: THREE.DoubleSide,
+    map: diffTexture,
+    // wireframe:true,
+    // opacity:0.5,
+    transparent:true,
+    // alphaMap:roughTexture,
+})
+loader.load( '/src/cup.glb', function ( gltf ) {
+    console.log()
+    gltf.scene.scale.x = 0.2
+    gltf.scene.scale.y = 0.2
+    gltf.scene.scale.z = 0.2
+    gltf.scene.position.y = -1
+    gltf.scene.rotation.y = Math.PI
+    gltf.scene.children[0].material = material
+    console.log(gltf.scene.children[0])
+    test.scene.add( gltf.scene );
+} );
